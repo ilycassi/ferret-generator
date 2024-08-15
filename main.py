ecd258088ac098a593a0d67dea7142a269fe8683
ilycassi <casrowland17@outlook.com>
2024-08-15 19:27:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import webbrowser
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+import pyperclip
 
 url = "https://ferrets.leodog896.com/v1/image/random"
-#ahhhhhhhhh
+
 def img_from_url():
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
@@ -21,8 +22,10 @@ def set_image():
     ferret_image=ImageTk.PhotoImage(img_from_url())
     label.configure(image=ferret_image)
     label.image=ferret_image
-botton=ttk.Button(root,text="click me",command=set_image)
-botton.pack(side="bottom",padx=10,pady=10)
+button=ttk.Button(root,text="copy link",command=pyperclip.copy(url))
+button.pack(side="bottom",padx=10,pady=10)
+button=ttk.Button(root,text="click me",command=set_image)
+button.pack(side="bottom",padx=10,pady=10)
 ferret_image=ImageTk.PhotoImage(img_from_url())
 label = ttk.Label(root, image=ferret_image)
 label.pack(fill=tkinter.BOTH, expand=True, padx=10, pady=10)
