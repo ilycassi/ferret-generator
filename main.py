a9099c31ec85f37c0ca74e210d829f81ff8d19bc
ilycassi <casrowland17@outlook.com>
2024-08-15 20:23:32 -0400

    add copy link

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import requests
 from io import BytesIO
 import pyperclip
 
-url = "https://ferrets.leodog896.com/v1/image/random"
+url = "https://ferrets.leodog896.com/v1/data/random"
+baseurl = "https://ferrets.leodog896.com"
+currenturl = ""
 
 def img_from_url():
     response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
+    imageurl=baseurl+response.json()['url']
+    print(imageurl)
+    global currenturl
+    currenturl = str (imageurl)
+    print (currenturl)
+    imagedata = requests.get(str (imageurl))
+    img = Image.open(BytesIO(imagedata.content))
     img = img.resize((720, 720))
     return img
+def copy_link():
+    global currenturl
+    pyperclip.copy(currenturl)
 root = tkinter.Tk()
 root.configure(bg="black")
 root.title("ferret generator")
@@ -22,7 +33,7 @@ def set_image():
     ferret_image=ImageTk.PhotoImage(img_from_url())
     label.configure(image=ferret_image)
     label.image=ferret_image
-button=ttk.Button(root,text="copy link",command=pyperclip.copy(url))
+button=ttk.Button(root,text="copy link",command=copy_link)
 button.pack(side="bottom",padx=10,pady=10)
 button=ttk.Button(root,text="click me",command=set_image)
 button.pack(side="bottom",padx=10,pady=10)
